cd1d4cc7ffa3e0de58051d98a459632f9e30047a
YareYareOjiSan <113853299+YareYareOjiSan@users.noreply.github.com>
2022-11-20 19:31:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let pessoa =
+{
+    nome: "Fabio",
+    idade: 20,
+};
+
+  console.log(pessoa.nome,pessoa.idade);
+
+  let idade2 = 7*pessoa.idade;
+
+  console.log(idade2)
+
+  let amigos = ["Saul Goodman","OJ Simpson","David Carson"]
+
+
+let estudantes = [
+    {
+      nome: "Saul Goodman",
+      idade: 48,
+      isStudent:false
+    },
+
+    {
+        nome: "OJ Simpson",
+        idade: 75,
+        isStudent:true
+    },
+
+    {
+        nome: "David Carson",
+        idade: 67,
+        isStudent:true
+    }];
+
+
+      console.log(nome.estudante,idade.estudante,isStudent.estudante);
